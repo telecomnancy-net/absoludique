ab765cdd88842f393a580dcca6c488b6de8c3f9c
rom100main <rom100main@proton.me>
2025-09-10 09:14:02 +0200

    fix: remove ai bg remover since dont work as of now with docker

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ from flask_login import (
     logout_user,
 )
 from PIL import Image
-import rembg
+# import rembg # as of now don't work with Docker, need more testing
 
 load_dotenv()
 
@@ -172,8 +172,8 @@ def find_user_name(name: str):
 
 
 def resize_and_convert_to_webp(file, max_width, max_height, quality=80):
-    img_data = Image.open(file)
-    img = rembg.remove(img_data)
+    img = Image.open(file)
+    # img = rembg.remove(img)
     
     original_width, original_height = img.size
     width_ratio = max_width / original_width
