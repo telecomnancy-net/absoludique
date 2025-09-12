56d6446ab047afffe3f5e828c4fd616724db8145
Lucas SIMONNEAU <lucas.simonneau@telecomnancy.eu>
2025-09-12 18:03:30 +0200

    fix: cherry-pick from main

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ from flask_login import (
     logout_user,
 )
 from PIL import Image
-from rembg import remove
+import rembg
 
 load_dotenv()
 
@@ -172,9 +172,9 @@ def find_user_name(name: str):
 
 
 def resize_and_convert_to_webp(file, max_width, max_height, quality=80):
-    img_data = Image.open(file)
-    img = remove(img_data)
-    
+    img = Image.open(file)
+    img = rembg.remove(img)
+
     original_width, original_height = img.size
     width_ratio = max_width / original_width
     height_ratio = max_height / original_height
